442f30a03e4236da71f89e13097896dec4693755
hhl <hhl206@qq.com>
2020-02-25 21:03:02 +0800

    md

---
+++ .vuepress/config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  base: '/rwind-python/',
+  dest: path.resolve(__dirname, '../docs'),
+  title: 'RWIND-Python',
+  description: 'Python document',
+  head: [
+    ['link', { rel: 'icon', href: `/favicon.ico` }]
+  ],
+  themeConfig: {
+    nav: [
+      { text: '基础', link: '/basic/' },
+      { text: 'Github', link: 'https://github.com/jason6833/rwind-python/' }
+    ],
+    displayAllHeaders: true,
+    sidebarDepth: 2,
+    sidebar: {
+    	"/basic/":[
+			{
+	      title: "Python",
+	        children: [
+              "./python/1.概述"
+	        ]
+        },
+    	]
+    }
+  }
+}
